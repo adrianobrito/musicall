b408cbcd2654d4edc5e0907c9db0e8829448b62e
Adriano Brito <adrianosdebrito@gmail.com>
2015-11-26 14:43:36 -0300

    Novos ajustes visuais

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,15 @@
 var go_to = function(link){
   location.href = link;
 }
+
+$(document).ready(function(){
+  $(".with-bar").mouseover(function(){
+    console.log("entrou");
+    this.innerHTML = '// ' + $(this).text();
+  });
+
+  $(".with-bar").mouseout(function(){
+    console.log("saiu");
+    $(this).text($(this).text().split('//')[1]);
+  });  
+})
